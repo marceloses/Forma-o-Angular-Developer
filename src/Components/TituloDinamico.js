726c1e1ee691e8911d9e5a37e269f5c07f3e412c
Marcelo Severo <git@github.com:marceloses/APICatalogo.git>
2025-04-01 18:30:09 -0300

    Estilizando Component

---
+++ src/Components/TituloDinamico.js
@@ -0,0 +1,13 @@
+class TituloDinamico extends HTMLElement {
+    constructor(){
+        super();
+
+        const shadow =this.attachShadow({mode: "open"});
+        // base do component
+        //estililizar o component
+        //enviar para o shadow
+
+    }
+}
+
+customElements.define('Titulo-dinamico')
